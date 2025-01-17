f03f26669010156fb14ee98e45adf322e7fa4d27
puja lakhe <lakhepuza924@gmail.com>
2025-01-17 22:36:40 +0545

    python string method

---
+++ string methods.py
@@ -0,0 +1,20 @@
+#String= Series of character
+
+#name=input("Enter Your Name")
+phone_number=input("Enter Your Phone Number")
+
+#result=len(name)   returns length of the name
+#result=name.find(" ")   finds space
+#result=name.find("a")    find a from the start        
+#result=name.rfind("e")       finds e from reverse
+#name=name.capitalize()        capitalize the first letter of the string 
+#name=name.upper()              capitalizes all letters of the string
+#name=name.lower()          lowercase all capital letters of the string 
+#result=name.isdigit()         returns true only if string contains digits
+#result=name.isalpha()          returns true only if the string doesnt contain any digits or space
+#result=phone_number.count("-")     counts the number of dash
+#phone_number=phone_number.replace("-"," ")   replaces the dash
+
+
+
+print(phone_number)
