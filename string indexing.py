1b6ab8da8ba1b2c234cddbe56fa86360b2c44ae0
puja lakhe <lakhepuza924@gmail.com>
2025-01-18 20:05:39 +0545

    string indexing

---
+++ string indexing.py
@@ -0,0 +1,15 @@
+#indexing: allows user to access elements using[]
+#    syntax:[start:end:step]  
+
+credit_number="1444-222889-67889-43"              
+
+
+#print(credit_number[6])     one field listed asssumed as starting position
+#print(credit_number[0:4])
+#print(credit_number[5:])       prints everything from index 5 to last index
+#print(credit_number[-5])           access form end
+#print(credit_number[::2])           prints every second character in the string
+
+#print(credit_number[-2:])
+#credit_number=credit_number[::-1]      reverse string
+print(credit_number)
