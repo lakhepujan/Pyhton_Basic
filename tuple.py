c7653bb127724c58a2b7623fcac40d26b908d76b
puja lakhe <lakhepuza924@gmail.com>
2025-01-20 22:29:25 +0545

    collection:tuple

---
+++ tuple.py
@@ -0,0 +1,11 @@
+fruits= ("apple", "bananas","orange","coconut")
+#print(dir(fruits)) provides attributes that can be used in list,set and tuple
+#print(help(fruits))  provides details of the attributes
+#print(len(fruits)) prints len of the collection variable
+#print("pineapple" in fruits)   checks whether the given element is in the list or not
+
+#print(fruits.index("apple"))   shows the index of the element
+#print(fruits.count("apple"))   counts the number of elements in  the list
+
+
+print(fruits)
