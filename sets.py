96b430a474118cb7f51fb2afb71ca04566374bf6
puja lakhe <lakhepuza924@gmail.com>
2025-01-20 22:29:36 +0545

    collection:sets

---
+++ sets.py
@@ -0,0 +1,12 @@
+fruits= {"apple", "bananas","orange","coconut"}
+#cannt use indexing in sets
+#print(dir(fruits)) provides attributes that can be used in list,set and tuple
+#print(help(fruits))  provides details of the attributes
+#print(len(fruits)) prints len of the collection variable
+#print("pineapple" in fruits)   checks whether the given element is in the list or not
+
+#fruits.add("pineapple")        add elements in the set
+#fruits.remove("apple")         remove element from the set
+fruits.pop()
+
+print(fruits)
