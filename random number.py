625e4d7a60b0c137df68fbbeac69dda6134da283
puja lakhe <lakhepuza924@gmail.com>
2025-01-27 20:24:48 +0545

    random numbers

---
+++ random number.py
@@ -0,0 +1,13 @@
+import random
+
+low=1
+high=100
+options=("paper", "rock" ,"scissors")
+cards=["2" ,"3", "4" ,"5", "6", "7", "K", "Q"," A","J"]
+
+#number= random.randint(1,10) prints random whole integer from the given range
+#number=random.random()     gives random floating point number
+#option=random.choice(options)  makes a choice from the given options
+#number = random.randint(low,high) can take variable if associated with numbers
+#random.shuffle(cards) shuffle the given options
+print(cards)
