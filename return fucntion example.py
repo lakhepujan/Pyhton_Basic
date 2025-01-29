9d87522673b468c369825dd0e370e1ff8d6495a5
puja lakhe <lakhepuza924@gmail.com>
2025-01-29 20:58:10 +0545

    return fuction example

---
+++ return fucntion example.py
@@ -0,0 +1,7 @@
+def create_name(f_name,l_name):
+    print(f"Your first name : {f_name}")
+    print(f"Your last name : {l_name}")
+    return f_name + " " +l_name
+
+full_name = create_name("Pujan" ,"Lakhe")
+print(full_name)
