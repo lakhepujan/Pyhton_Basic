3c9524452225278ac3868573ce6c3298e85b91cf
puja lakhe <lakhepuza924@gmail.com>
2025-01-29 20:58:45 +0545

    function example

---
+++ function example.py
@@ -0,0 +1,6 @@
+def payment_due(username,amount,due_date):
+    print(f"Hello {username}")
+    print(f"Your due amount is {amount}")
+    print(f"The due amouont should be paid in {due_date}")
+
+payment_due("Rahul",200,"07/09")
