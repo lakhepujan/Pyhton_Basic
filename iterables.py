03e67c5a865bab41db775b9a4d63de9e5f7e45eb
puja lakhe <lakhepuza924@gmail.com>
2025-02-02 21:10:57 +0545

    iterables

---
+++ iterables.py
@@ -0,0 +1,22 @@
+# #numbers = (1,2,3,4,5)
+
+#  for num in numbers:
+#     print (num, end=" ") #can be reversed
+#                         #tuple alsoo itearble
+
+# fruits ={"apple","orange","banana"}  #set not reversabel
+
+# for item in fruits:
+#     print(item,end=" ")
+
+# name = "Puzan"
+
+# for item in name:
+#     print(item)
+
+my_dictationary={"A" : 1,
+                 "B" : 2,
+                 "C" : 3}
+
+for key,value in my_dictationary.items():
+    print(f"{key} :{value}")
