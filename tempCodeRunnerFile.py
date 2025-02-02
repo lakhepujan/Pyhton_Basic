ad24e6cc9613dc61ef58dad08814787060df3f61
puja lakhe <lakhepuza924@gmail.com>
2025-02-02 21:12:01 +0545

    keyword arbitary and argument arbitary

---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,24 @@
+def shipping_label(*args,**kwargs):
+    for arg in args:
+        print(arg , end=" ")
+    print()
+    if "apt" in kwargs:
+        print(f"{kwargs.get('street')} {kwargs.get('apt')}")
+    elif "pobox" in kwargs:
+        print(f"{kwargs.get('street')} {kwargs.get('pobox')}")
+    else:
+         print(f"{kwargs.get('street')}")
+
+         print(f"{kwargs.get('city')} {kwargs.get('state')} {kwargs.get('zip')}")
+        
+    
+
+
+
+
+shipping_label("Dr","Spongebob","Squarepants",
+            street="123 Fake St",
+            apt="100",
+            city="Detorit",
+            state="MI",
+            zip="546")
