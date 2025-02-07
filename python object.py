9174b172b473c0fbe4b6f07bb4428604c5a2d046
puja lakhe <lakhepuza924@gmail.com>
2025-02-07 13:59:11 +0545

    python object oriented program

---
+++ python object.py
@@ -0,0 +1,12 @@
+from car_code import Car 
+
+car1 = Car("Mustang", 2024,"Red",False)
+car2=Car("Corvette", 2025,"Bule",True)
+# print(car2.model)
+# print(car2.year)
+# print(car2.color)
+# print(car2.for_sale)
+
+car1.describe()
+
+        
