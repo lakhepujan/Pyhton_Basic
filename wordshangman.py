ceb770bde4ecf450c3db0c77f08a34e9219d8081
puja lakhe <lakhepuza924@gmail.com>
2025-02-07 13:58:27 +0545

    hangman module

---
+++ wordshangman.py
@@ -0,0 +1,12 @@
+words = (
+    "aardvark", "alligator", "alpaca", "antelope", "armadillo", "baboon", "badger", "barracuda", "bat", "bear",
+    "beaver", "bison", "boar", "buffalo", "butterfly", "camel", "capybara", "caribou", "cassowary", "cat",
+    "caterpillar", "chameleon", "cheetah", "chimpanzee", "chinchilla", "cobra", "cockatoo", "cougar", "coyote", "crab",
+    "crane", "crocodile", "crow", "deer", "dingo", "dolphin", "donkey", "dove", "dragonfly", "duck",
+    "eagle", "echidna", "eel", "elephant", "emu", "falcon", "ferret", "flamingo", "fox", "frog",
+    "gazelle", "gecko", "giraffe", "goat", "goose", "gorilla", "grasshopper", "hamster", "hare", "hawk",
+    "hedgehog", "heron", "hippopotamus", "hornet", "horse", "hummingbird", "hyena", "ibex", "ibis", "iguana",
+    "jackal", "jaguar", "jellyfish", "kangaroo", "kingfisher", "koala", "koi", "krill", "lemur", "leopard",
+    "lion", "lizard", "llama", "lobster", "lynx", "macaw", "magpie", "manatee", "mandrill", "meerkat",
+    "mole", "mongoose", "monkey", "moose", "narwhal", "newt", "ocelot", "octopus", "opossum", "orangutan",
+    "otter", "owl", "oyster", "panda", "panther", "parrot", "peacock", "pelican", "penguin", "platypus")
